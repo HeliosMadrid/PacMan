423663a358959f5b43b15f5d9446a4f692308cbb
Gabriel <gabwill.anne.jeux@gmail.com>
2022-10-21 20:58:50 +0200

    maintenant c bo

--- PacMan.py
+++ PacMan.py
@@ -74,9 +74,9 @@ def update():
     # charge la grid si elle n'est pas déjà chargée
     if not grid:
         loadGrid()
-        pac_man = PacMan(13, 13, WIDTH, HEIGHT, len(grid[0]), len(grid))
-        blinky = Blinky(4, 13, WIDTH, HEIGHT, len(grid[0]), len(grid))
-        clyde = Clyde(7, 25, WIDTH, HEIGHT, len(grid[0]), len(grid))
+        pac_man = PacMan(12, 13, WIDTH, HEIGHT, len(grid[0]), len(grid))
+        blinky = Blinky(12, 21, WIDTH, HEIGHT, len(grid[0]), len(grid))
+        clyde = Clyde(12, 21, WIDTH, HEIGHT, len(grid[0]), len(grid))
 
     # gère les inputs du joueur
     if keyboard.UP:
